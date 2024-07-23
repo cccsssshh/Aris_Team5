44ea19cd231706ed79cb3c19d6f4a41b9e34911d
kgt6467 <kgt6467@gmail.com>
2024-07-23 10:25:53 +0900

    add kiosk package

---
+++ src/kiosk_package/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'kiosk_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/ui', ['ui/kiosk.ui'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='k',
+    maintainer_email='kgt6467@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'kiosk = kiosk_package.kiosk:main'
+        ],
+    },
+)
