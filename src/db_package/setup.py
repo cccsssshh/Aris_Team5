e27bf2e7d3038b219a074e864660a5cf978bf6c2
cccsssshh <shcho9666@gmail.com>
2024-08-01 13:07:19 +0900

    refactorying db

--- src/db_package/setup.py
+++ src/db_package/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'db = db_package.db:main'
+            'db = db_package.main:main'
         ],
     },
 )
